a47f341979dc8e9f6c4652f113c48c1444e34365
Sarah Kim <sarahkim@Sarahs-M1-MacBook-Pro.local>
2022-03-01 21:57:54 -0500

    Added winning word constant. Updated word bank

--- js/main.js
+++ js/main.js
@@ -16,16 +16,24 @@ const wordList = [
     hint: "COLOUR OF FRY'S HAIR"},
 
     {word: "SLURM",
-    hint: "NAME OF FRY'S FAVOURITE DRINK"}
+    hint: "NAME OF FRY'S FAVOURITE DRINK"},
+
+    {word: "MARS",
+    hint: "WHAT PLANET DOES AMY'S PARENTS LIVE ON?"},
+
+
 ];
 
-// start with default first word
-const currentWord = wordList[1].word;
-console.log(currentWord);
+// generates a random number between 1 and the length of the wordList bank to choose which word will be guessed
+
+const winningWord = wordList[Math.floor(Math.random() * wordList.length)].word;
+console.log(winningWord);
+
+
 
 // create loop to equate to the number of boxes and length of word
 function addBoxes() {
-    for(let i = 0; i < currentWord.length-1; i++) {
+    for(let i = 0; i < winningWord.length-1; i++) {
         createBox();
     };
 };
@@ -48,9 +56,26 @@ const keyDown = document.addEventListener("keydown", checkLetter);
 // if +# = the position of the letter in the string
 function checkLetter(e) {
     console.log(e);
-    const indexOfLetter = currentWord.indexOf(e.key.toUpperCase());
+    const indexOfLetter = winningWord.indexOf(e.key.toUpperCase());
     console.log(indexOfLetter);
 };
+
+// Check if letter matches current (winning) word
+
+function checkWin() {
+    if(checkLetter >= 0) {
+        const addLetter = document.querySelector(".boxes").innerHTML = "A";
+        // let letter at the at index be displayed in the boxes and highlight letter green
+    } else {
+        // remove a life and highlight letter red
+    }; 
+};
+
+checkWin();
+
+
+
+
 
 // // app's state (variables)
 // let lose = numGuesses > maxGuesses;
